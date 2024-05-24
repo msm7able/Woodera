c3c33164e713578bb196916de10865dbfb7451de
Monica Bob <monicamihaela.bob@gmail.com>
2024-05-24 16:19:42 +0300

    GetStartedJs

---
+++ index.js
@@ -0,0 +1,8 @@
+// Selectăm butonul "Get Started"
+const startButton = document.getElementById("startButton");
+
+// Adăugăm un eveniment de clic
+startButton.addEventListener("click", function () {
+  // Afisăm un mesaj de salut
+  alert("Bun venit la WOODERA! Începeți să trimiteți mesaje nelimitate acum!");
+});
