79acfc9fb39a9de9a4559f9fa87671c4464d6f8b
Monica Bob <monicamihaela.bob@gmail.com>
2024-05-27 14:38:19 +0300

    update camp de email

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-// Selectăm butonul "Get Started"
 const startButton = document.getElementById("startButton");
 
-// Adăugăm un eveniment de clic
 startButton.addEventListener("click", function () {
   // Scroll la secțiunea 1
   document.getElementById("section1").scrollIntoView({ behavior: "smooth" });
 });
 
-// Selectăm câmpul de email
+// Câmpul de email
 const emailInput = document.getElementById("emailInput");
 
-// Adăugăm un eveniment de clic pentru câmpul de email
 emailInput.addEventListener("focus", function () {
-  // Afișăm un mesaj de informare
   emailInput.placeholder = "";
 });
 
 emailInput.addEventListener("blur", function () {
-  // Restaurăm placeholder-ul dacă câmpul este gol
   if (!emailInput.value) {
     emailInput.placeholder = "youremail@gmail.com";
   }
