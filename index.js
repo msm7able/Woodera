d7ff5719d4b137dcbd450f63b0dc4b505e58d1ae
Monica Bob <monicamihaela.bob@gmail.com>
2024-05-27 14:06:19 +0300

    Sectiune 2

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const startButton = document.getElementById("startButton");
 
 // Adăugăm un eveniment de clic
 startButton.addEventListener("click", function () {
-  // Afisăm un mesaj de salut
-  alert("Welcome to Woodera! Bringing wood to your homes!");
+  // Scroll la secțiunea 1
+  document.getElementById("section1").scrollIntoView({ behavior: "smooth" });
+});
+
+// Selectăm câmpul de email
+const emailInput = document.getElementById("emailInput");
+
+// Adăugăm un eveniment de clic pentru câmpul de email
+emailInput.addEventListener("focus", function () {
+  // Afișăm un mesaj de informare
+  emailInput.placeholder = "";
+});
+
+emailInput.addEventListener("blur", function () {
+  // Restaurăm placeholder-ul dacă câmpul este gol
+  if (!emailInput.value) {
+    emailInput.placeholder = "youremail@gmail.com";
+  }
 });
